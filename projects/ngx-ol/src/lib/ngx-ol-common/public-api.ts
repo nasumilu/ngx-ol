d1119cbac6dba6bd5a6388a7e76f986ad2b21d3b
Michael Lucas <nasumilu@gmail.com>
2023-07-04 08:01:46 -0400

    fixed modules exports

--- projects/ngx-ol/src/lib/ngx-ol-common/public-api.ts
+++ projects/ngx-ol/src/lib/ngx-ol-common/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-ol-common
  */
 
-export * from './transform';
-export * from './coord-format.pipe';
 export * from './common.module';
+export * from './coordinate.pipe';
+export * from './transform.pipe';
+export * from './transform';
